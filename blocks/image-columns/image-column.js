3eb3cc5c108ab5c3a36b695931d17d331a798b86
sagarkbhatt <sagarkbhatt95@gmail.com>
2018-02-13 14:59:15 +0530

    Add image column blocks

---
+++ blocks/image-columns/image-column.js
@@ -0,0 +1,93 @@
+/**
+ * Contains Image Column
+ */
+
+const { __ } = wp.i18n;
+const { Component } = wp.element;
+const { RichText, MediaUpload } = wp.blocks;
+const { Button, IconButton, Placeholder } = wp.components;
+
+class ImageColumn extends Component {
+	constructor() {
+		super( ...arguments );
+	}
+
+	render() {
+		const { attributes, focused, setFocus, index, onRemove } = this.props;
+		const focusedRichText = focused ? focused.RichText || `${ index }-title` : null;
+
+		return (
+			<div className={ this.props.className } key="image-columns-container" >
+				{ attributes.mediaID && (
+					<figure>
+						<IconButton
+							key="icon-button"
+							icon="no-alt"
+							onClick={ onRemove }
+							className="rt-remove-image-button"
+							label={ __( 'Remove Image' ) }
+						/>
+						<img src={ attributes.mediaURL } alt="" />
+					</figure>
+				) }
+				{ ! attributes.mediaID && (
+					<Placeholder
+						key="placeholder"
+						icon="media-image"
+						label={ __( 'Thumbnail' ) }
+						instructions={ __( 'Upload or choose from media library' ) }
+						className="rt-image-placeholder">
+						<MediaUpload
+							onSelect={ this.props.onSelectImage }
+							type="image"
+							value={ attributes.mediaID }
+							render={ ( { open } ) => (
+								<Button key="button" className={ attributes.mediaID ? 'image-button' : 'button button-large' } onClick={ open } >
+									{ ! attributes.mediaID ? __( 'Choose' ) : '' }
+								</Button>
+							) }
+						/>
+					</Placeholder>
+				) }
+				<RichText
+					tagName="h3"
+					onChange={ this.props.onChangeTitle }
+					value={ attributes.title }
+					placeholder={ __( 'Enter Title...' ) }
+					focus={ focusedRichText === `${ index }-title` }
+					onFocus={ ( focus ) => setFocus( _.extend( {}, focus, { RichText: `${ index }-title` } ) ) }
+				/>
+				{ this.props.showSubHeading && (
+					<RichText
+						onChange={ this.props.onChangeSubTitle }
+						value={ attributes.subHeading }
+						placeholder={ __( 'Enter Sub Title...' ) }
+						focus={ focusedRichText === `${ index }-sub-title` }
+						onFocus={ ( focus ) => setFocus( _.extend( {}, focus, { RichText: `${ index }-sub-title` } ) ) }
+						inlineToolbar
+					/>
+				) }
+				<RichText
+					onChange={ this.props.onChangeContent }
+					value={ attributes.content }
+					placeholder={ __( 'Enter Content...' ) }
+					focus={ focusedRichText === `${ index }-content` }
+					onFocus={ ( focus ) => setFocus( _.extend( {}, focus, { RichText: `${ index }-content` } ) ) }
+					inlineToolbar
+				/>
+				{ this.props.showReadMore && (
+					<RichText
+						onChange={ this.props.onChangeReadMore }
+						value={ attributes.readMore ? attributes.readMore : __( 'Read More' ) }
+						placeholder={ __( 'Read More Text and Link...' ) }
+						focus={ focusedRichText === `${ index }-readmore` }
+						onFocus={ ( focus ) => setFocus( _.extend( {}, focus, { RichText: `${ index }-readmore` } ) ) }
+						inlineToolbar
+					/>
+				) }
+			</div>
+		);
+	}
+}
+
+export default ImageColumn;
---
+++ blocks/image-columns/index.js
@@ -0,0 +1,117 @@
+/**
+ * Registers bio block.
+ */
+
+import ImageColumnBlock from './block';
+
+const { __ } = wp.i18n;
+const { registerBlockType } = wp.blocks;
+
+registerBlockType( 'rtgb/image-columns', {
+	title: __( 'Image Columns' ),
+	icon: 'index-card',
+	category: 'layout',
+
+	attributes: {
+		columns: {
+			type: 'array',
+			source: 'query',
+			selector: '.rt-column',
+			query: {
+				mediaID: {
+					type: 'number',
+					source: 'attribute',
+					selector: '.rt-header-content img',
+					attribute: 'data-media-id',
+				},
+				mediaURL: {
+					type: 'string',
+					source: 'attribute',
+					selector: '.rt-header-content img',
+					attribute: 'src',
+				},
+				title: {
+					source: 'children',
+					selector: '.rt-column-title',
+				},
+				subHeading: {
+					source: 'children',
+					selector: '.rt-column-sub-heading',
+				},
+				content: {
+					source: 'children',
+					selector: '.rt-column-content',
+				},
+				readMore: {
+					source: 'children',
+					selector: '.rt-read-more',
+				},
+			},
+			default: [ {}, {}, {} ],
+		},
+		columnCount: {
+			type: 'number',
+			default: 3,
+		},
+		showSubHeading: {
+			type: 'boolean',
+			default: false,
+		},
+		showReadMore: {
+			type: 'boolean',
+			default: true,
+		},
+	},
+
+	edit: ImageColumnBlock,
+
+	save: props => {
+		const columns = props.attributes.columns || [];
+		const columnCount = props.attributes.columnCount;
+		const className = props.className;
+		const imageColumns = [];
+
+		if ( ! columns.length ) {
+			return null;
+		}
+
+		_.each( columns, function( column, index ) {
+			let columnClass = `rt-column rt-column-${ index }`;
+			let columnKey = `rt-column-${ index }`;
+
+			if ( index + 1 > columnCount ) {
+				return;
+			}
+
+			imageColumns.push(
+				<li key={ columnKey } className={ columnClass }>
+					<figure className="rt-header-content">
+						<img src={ column.mediaURL } data-media-id={ column.mediaID } alt="" />
+					</figure>
+					<h3 className="rt-column-title" >
+						{ column.title }
+					</h3>
+					{ props.attributes.showSubHeading && (
+						<p className="rt-column-sub-heading">
+							{ column.subHeading }
+						</p>
+					) }
+					<div className="rt-column-content">
+						{ column.content }
+					</div>
+					{ props.attributes.showReadMore && (
+						<div className="rt-read-more">
+							{ column.readMore }
+						</div>
+					) }
+				</li>
+			);
+		} );
+
+		return (
+			<ul className={ className } key="rt-image-columns" >
+				{ imageColumns }
+			</ul>
+		);
+	},
+} );
