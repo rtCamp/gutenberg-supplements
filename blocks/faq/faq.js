ea61c875339513499a1c68764e4cc7e28b49170e
Siddharth <siddharth.tikekar@rtcamp.com>
2018-02-26 18:06:25 +0530

    Fixed toggle class bug on front-end

--- blocks/faq/faq.js
+++ blocks/faq/faq.js
@@ -1,12 +1,12 @@
 jQuery(document).ready(function(){
     jQuery('h4.faq-question').click(function(e){
-        this.classList.toggle('faq-active');
 
         var panel = this.nextSibling.nextSibling;
 
         if( panel.style.maxHeight !== '12em' ){
 
-            jQuery(this).parent().find('.faq-panel').css({'max-height':'0','min-height':'0'});
+            jQuery(this).parent().find('div.faq-panel').css({'max-height':'0','min-height':'0'});
+            jQuery(this).parent().find('h4.faq-question').removeClass('faq-active');
 
             panel.style.maxHeight = '12em';
             panel.style.minHeight = '12em';
@@ -15,6 +15,8 @@ jQuery(document).ready(function(){
             panel.style.maxHeight = '0';
             panel.style.minHeight = '0';
         }
+
+        this.classList.toggle('faq-active');
 
     });
 });
