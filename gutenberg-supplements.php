021d1da87f7fe37c704ada7c1b5206eaa4cb9062
manish.songirkar <manish.songirkar@rtcamp.com>
2018-03-14 15:08:46 +0530

    Add plugin dir path

--- gutenberg-supplements.php
+++ gutenberg-supplements.php
@@ -24,6 +24,10 @@ if ( ! defined( 'RT_GS_DIR_URL' ) ) {
 	define( 'RT_GS_DIR_URL', plugin_dir_url( __FILE__ ) );
 }
 
+if ( ! defined( 'RT_GS_DIR_PATH' ) ) {
+	define( 'RT_GS_DIR_PATH', plugin_dir_path( __FILE__ ) );
+}
+
 if ( ! defined( 'ABSPATH' ) ) {
 	return;
 }
